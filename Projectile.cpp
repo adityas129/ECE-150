595827b5f6b502f302db1c4d41305bc05c10c0a9
Aditya Sharma <adityas129@gmail.com>
2017-11-02 19:14:20 -0400

    here we go with assingment 6! Things are starting to get interesting. Physics implementation; string-to-int stuff and i/o

---
+++ Projectile.cpp
@@ -0,0 +1,82 @@
+//////////////////////////////////////////////////////////////
+//
+// Your #includes here; make sure you are allowed them ...
+//
+
+#include <stdlib.h>
+
+//////////////////////////////////////////////////////////////
+//
+// #includes and function declarations; do not remove
+//
+
+#ifndef MARMOSET_TESTING
+#include <iostream>
+using namespace std;
+#endif
+
+//////////////////////////////////////////////////////////////
+//
+// Function declarations; do not remove
+// Replace "..." in bisectionHelper() with any parameter(s)
+// you need to ensure termination when precision and
+// minIntervalSize are too tightly specified.
+//
+
+bool projectileDestination(const float h, const float v, const float m,
+                           const float theta, const float d,
+                           const float t, const float b, const float w,
+                           float& destX, float& destY);
+
+//////////////////////////////////////////////////////////////
+//
+// Your code here ...
+//  
+// Return std::numeric_limits<float>::quiet_NaN() (comes from
+// <limits>) if cannot compute the root for some reason
+//
+
+bool projectileDestination(const float h, const float v, const float m,
+                           const float theta, const float d,
+                           const float t, const float b, const float w,
+                           float& destX, float& destY) {
+  // Your code here ...
+}
+
+//////////////////////////////////////////////////////////////
+//
+// Test Driver
+//
+// Do not remove the #ifndef and its associated #endif
+// This is required so that when you submit your code,
+// this test driver will be ignored by Marmoset
+//
+
+#ifndef MARMOSET_TESTING
+
+#define isNaN(X) (X != X)  // NaN is the only float that is not equal to itself
+
+int main(const int argc, const char* const argv[]) {
+  
+  // Some test driver code here ....
+
+  float h = 10;
+  float v = 10;
+  float m = 10;
+  float theta = 45;        // Angle in degrees; will need to be converted by function
+  float d = 100;
+  float t = 5;
+  float b = 97;
+  float w = 5;
+
+  float hitsAtX;
+  float hitsAtY;
+
+  if (projectileDestination(h,v,m,theta,d,t,b,w,hitsAtX,hitsAtY))
+    cout << "Projectile hit at (" << hitsAtX << ", " << hitsAtY << ")" <<endl;
+  else
+    cout << "Unable to calculate where projectile hits." << endl;
+  return 0;
+}
+
+#endif
