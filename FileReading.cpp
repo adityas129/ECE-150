b5a82d0b446c536f70e276dba82bf6e36071d8d5
Aditya Sharma <adityas129@gmail.com>
2017-11-02 19:46:49 -0400

    assingment 6 adding 3rd task

---
+++ FileReading.cpp
@@ -0,0 +1,94 @@
+//////////////////////////////////////////////////////////////
+//
+// Your #includes here; make sure you are allowed them ...
+//
+
+#include <stdlib.h>
+
+//////////////////////////////////////////////////////////////
+//
+// #includes and function declarations; do not remove
+//
+
+#ifndef MARMOSET_TESTING
+#include <iostream>
+using namespace std;
+#endif
+
+//////////////////////////////////////////////////////////////
+//
+// Function declarations; do not remove
+// Replace "..." in bisectionHelper() with any parameter(s)
+// you need to ensure termination when precision and
+// minIntervalSize are too tightly specified.
+//
+
+int histogram(const char filename[], int histogram[10], 
+              const int minAcceptableID, const int maxAcceptableID,
+              int*& rejects);
+
+//////////////////////////////////////////////////////////////
+//
+// Your code here ...
+//  
+// Return -1 if you cannot compute the histogram for any
+// reason;
+// Return 0 if the histogram is correct and there are no
+// rejections;
+// Return the number of rejects if there are rejected
+// records from the file
+//
+
+int histogram(const char filename[], int histogram[10], 
+              const int minAcceptableID, const int maxAcceptableID,
+              int*& rejects) {
+  // Your code here ...
+}
+
+//////////////////////////////////////////////////////////////
+//
+// Test Driver
+//
+// Do not remove the #ifndef and its associated #endif
+// This is required so that when you submit your code,
+// this test driver will be ignored by Marmoset
+//
+
+#ifndef MARMOSET_TESTING
+
+#define isNaN(X) (X != X)  // NaN is the only float that is not equal to itself
+
+int main(const int argc, const char* const argv[]) {
+  
+  // Some test driver code here ....
+  if (argc < 2) {
+
+  }
+  const int numBuckets = 10;
+  const int bucketRange = 100/numBuckets;
+  int  buckets[numBuckets];
+  int* rejectedRecords;
+  int retCode = histogram(argv[1], buckets, 22200000, 22299999, rejectedRecords);
+  if (retCode < 0) {
+      cout << "Unable to compute histogram." << endl;
+  }
+  else {
+    for (int i = 0; i < numBuckets; ++i)
+      if (i != numBuckets-1)
+	cout << "Number [" << i*bucketRange << "," << (((i+1)*bucketRange)-1) << "]: " << buckets[i] << endl;
+      else
+	cout << "Number [" << i*bucketRange << "," << (((i+1)*bucketRange)) << "]: " << buckets[i] << endl;
+    if (retCode > 0) {
+      cout << "Number of rejected records: " << retCode << endl;
+      cout << "Rejected records are:";
+      for (int i = 0; i < retCode; ++i)
+	cout << " " << rejectedRecords[i];
+      cout << endl;
+      delete[] rejectedRecords;
+    }
+  }
+
+  return 0;
+}
+
+#endif
